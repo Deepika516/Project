7e431e347352c8a2bd2aba42e2fa4610a7e0af4a
Deepika Mahindroo <deepika.mahindroo@sourcefuse.com>
2022-11-23 17:02:32 +0530

    ApplyingGaurds

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { NgxPermissionsModule } from 'ngx-permissions'; 
+import { NgxPermissionsService } from 'ngx-permissions/lib/service/permissions.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { AuthorizationService } from 'src/app/services/authorization.service';
+import { Role } from 'src/app/enums/roleEnum.enum';
 
 
 @Component({
@@ -9,15 +13,28 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isUserLoggedIn$!: Observable<boolean>;
+  isLogin: boolean = false;
+  
 
-  constructor(private router:Router) { }
+
+  constructor(private router:Router,private authorizationService:AuthorizationService) { }
+  
 
   ngOnInit(): void {
+    this.isUserLoggedIn$ = this.authorizationService.isLoggedIn;
+    // this.getPermissions();
   }
+
+  // getPermissions(){
+  //   const roles=["Admin","User"]
+  //   this.permissionService.loadPermissions(roles);
+  // }
 
   onLogout()
   {
-      this.router.navigate(['/']);
+      // this.isLogin = false;
+      this.authorizationService.logout();
       localStorage.removeItem("currentUser");
   }
 }
