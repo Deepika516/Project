17e8ad16568152ea6fc1c16b171c0e4cc77706e7
Deepika Mahindroo <deepika.mahindroo@sourcefuse.com>
2022-12-12 18:17:18 +0530

    TestServiceShow

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -6,71 +6,85 @@ import { IDept, IDoc } from 'src/app/interfaces/dept.interface';
 import { IAppointment } from 'src/app/interfaces/userAppointment.interface';
 import { UserService } from 'src/app/services/user.service';
 
-
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.css']
+  styleUrls: ['./appointment.component.css'],
 })
 export class AppointmentComponent implements OnInit {
-  
-  public appointmentForm !: FormGroup
-  department:IDept[]=[];
-  doctors:IDoc[]=[];
-  
-  constructor(private userService:UserService,private formBuilder:FormBuilder ,private router:Router) { }
+  public appointmentForm!: FormGroup;
+  department: IDept[] = [];
+  doctors: IDoc[] = [];
 
-   
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
   ngOnInit(): void {
     this.getDept();
-    this.appointmentForm=this.formBuilder.group({
-      name:[""],
-      dob:[""],
-      gender:[""],
-      email:[""],
-      dept:[""],
-      doc:[""],
-      doa:[""]
-    })
+    this.appointmentForm = this.formBuilder.group({
+      name: [''],
+      dob: [''],
+      gender: [''],
+      email: [''],
+      dept: [''],
+      doc: [''],
+      doa: [''],
+    });
   }
 
-  // to submit the appointment details and and check the details  
-  onSubmitAppointment(){
+  // to submit the appointment details and and check the details
+  onSubmitAppointment() {
     {
       if (this.appointmentForm.invalid) {
         return;
+      }
+      const user_name = this.appointmentForm.value.name;
+      const user_gen = this.appointmentForm.value.gender;
+      const user_dob = this.appointmentForm.value.dob;
+      const user_email = this.appointmentForm.value.email;
+      const user_dept =
+        this.department.find((e) => e.id == this.appointmentForm.value.dept)
+          ?.dept_name || '';
+      const user_doc = this.appointmentForm.value.doc;
+      const user_doa = this.appointmentForm.value.doa;
+      this.userService
+        .onAppointment(
+          user_name,
+          user_gen,
+          user_email,
+          user_dob,
+          user_dept,
+          user_doc,
+          user_doa
+        )
+        .subscribe((respond: IAppointment[]) => {
+          alert('Booking Successful');
+          this.router.navigate(['/appointment-check']);
+        });
     }
-    const user_name=this.appointmentForm.value.name;
-    const user_gen=this.appointmentForm.value.gender;
-    const user_dob=this.appointmentForm.value.dob;
-    const user_email=this.appointmentForm.value.email;
-    const user_dept=this.appointmentForm.value.dept;
-    const user_doc=this.appointmentForm.value.doc;
-    const user_doa=this.appointmentForm.value.doa;
-    this.userService.onAppointment(user_name,user_gen,user_email,user_dob,user_dept,user_doc,user_doa).subscribe((respond:IAppointment[])=>
-    {
-      alert("Booking Successful");
-      this.router.navigate(['/appointment-check'])
-
-    })
-  }
   }
 
   //to show department in the dropdown
-  getDept()
-  {
-    this.userService.showDept().pipe(take(1)).subscribe((respData:IDept[])=>{
-    this.department=respData;
-    });
+  getDept() {
+    this.userService
+      .showDept()
+      .pipe(take(1))
+      .subscribe((respData: IDept[]) => {
+        this.department = respData;
+      });
   }
- 
-  //on the click of department change event click and through department id doctors will show in dependent dropdown
-  changeDept(event:any){
-  let deptId=event.target.value;
-    this.userService.showDoc(+deptId).pipe(take(1))
-    .subscribe((docData:IDoc[])=>{
-      this.doctors=docData.filter(e=>e.dept_id===+deptId)
-  })
-}
-}
 
+  //on the click of department change event click and through department id doctors will show in dependent dropdown
+  changeDept(event: any) {
+    let deptId = event.target.value;
+    this.userService
+      .showDoc(+deptId)
+      .pipe(take(1))
+      .subscribe((docData: IDoc[]) => {
+        this.doctors = docData.filter((e) => e.dept_id === +deptId);
+      });
+  }
+}
--- src/app/components/lab-tests/lab-tests.component.ts
+++ src/app/components/lab-tests/lab-tests.component.ts
@@ -2,83 +2,114 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { InjectSetupWrapper } from '@angular/core/testing';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { IDiagnosis, IService, ISubService, ITest } from 'src/app/interfaces/userTestBooing.interface';
+import { CellClickedEvent, SelectionChangedEvent } from 'ag-grid-community';
+import { DropListener } from 'ag-grid-community/dist/lib/headerRendering/columnDrag/bodyDropTarget';
+import {
+  IDiagnosis,
+  IService,
+  ISubService,
+  ITest,
+} from 'src/app/interfaces/userTestBooing.interface';
 import { TestService } from 'src/app/services/test.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-lab-tests',
   templateUrl: './lab-tests.component.html',
-  styleUrls: ['./lab-tests.component.css']
+  styleUrls: ['./lab-tests.component.css'],
 })
 export class LabTestsComponent implements OnInit {
-
-  constructor(private userService:UserService,private http:HttpClient,private testService:TestService, private formBuilder: FormBuilder) {
+  constructor(
+    private userService: UserService,
+    private http: HttpClient,
+    private testService: TestService,
+    private formBuilder: FormBuilder
+  ) {
     this.getAllDiagnosisData();
-   }
-   contentDropdown:boolean=false;
-  public testForm!:FormGroup
-  public userName!:boolean
-  testList:ITest[]=[];
-  serviceList:IService[]=[];
-  subServiceList:ISubService[]=[];
-  checkboxList=[]
-  ngOnInit(): void {
-    this.testForm=this.formBuilder.group((
-      {
-        name:[""],
-        email:[""],
-        testName:[""],
-        testService:[""],
-        subService:[""],
-        date:[""]
-      }
-    ))
-    }
-
-    onCheckboxChange(event:any){
-      // this.checkboxList=this.testForm.value.subService
-      const checkId=event.target.value;
-      const isChecked=event.target.checked;
-      console.log(checkId);
-    }
-
-    getAllDiagnosisData(){
-      this.testList=this.testService.getTest();
-      // this.serviceList=this.testService.getTestServices();
-       console.log(this.testList);
-    }
-
-    changeDiagnosis(event:any){
-      debugger
-      let testId=event.target.value;
-      this.serviceList=this.testService.getTestServices().filter(t=>t.test_id==testId)
-    }
-
-    changeService(event:any){
-      debugger
-      let serviceId=event.target.value;
-      this.subServiceList=this.testService.getSubService().filter(u=>u.service_id==serviceId)
-    }
-
-    onSubmitTest(){
-      if(this.testForm.invalid)
-      {
-        return;
-      }
-      debugger
-      const user_t_name=this.testForm.value.name;
-      const user_t_email=this.testForm.value.email;
-      const user_t_testName=this.testForm.value.testName;
-      const user_t_testServiceName=this.testForm.value.testService;
-      const user_t_subserviceName=this.testForm.value.testService;
-      const user_t_date=this.testForm.value.date;
-      this.userService.getTestData(user_t_name,user_t_email,user_t_testName,user_t_testServiceName,user_t_subserviceName,user_t_date).subscribe((respData:IDiagnosis[])=>
-      {
-        alert("Test Appointment Successfull")
-      console.log(respData);
-      }
-      )
-
   }
+  contentDropdown: boolean = false;
+  public testForm!: FormGroup;
+  public userName!: boolean;
+  testList: ITest[] = [];
+  serviceList: IService[] = [];
+  subServiceList: ISubService[] = [];
+  checkboxList = [];
+  tempTestName = '';
+  ngOnInit(): void {
+    this.testForm = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      testName: [''],
+      testService: [''],
+      subService: [''],
+      date: [''],
+    });
+  }
+
+  // To get all the Test Name in Radio Buttons
+  getAllDiagnosisData() {
+    this.testList = this.testService.getTest();
+    // this.serviceList=this.testService.getTestServices();
+    console.log(this.testList);
+  }
+
+  //To change the test calling change event and shows dependent services in dropdown
+  changeDiagnosis(event: any) {
+    debugger;
+    let testId = event.target.value;
+    this.serviceList = this.testService
+      .getTestServices()
+      .filter((t) => t.test_id == testId);
+  }
+
+  //to chnage the service   calling change event and shows dependent subservices in dropdown
+  changeService(event: any) {
+    debugger;
+    let serviceId = event.target.value;
+    this.subServiceList = this.testService
+      .getSubService()
+      .filter((u) => u.service_id == serviceId);
+    console.log(this.subServiceList);
+  }
+
+  onSubmitTest() {
+    if (this.testForm.invalid) {
+      return;
+    }
+    debugger;
+    const user_t_name = this.testForm.value.name;
+    const user_t_email = this.testForm.value.email;
+    const user_t_testName = this.testForm.value.testName;
+
+    const user_t_testServiceName =
+      this.serviceList.find(
+        (e) => e.service_id == this.testForm.value.testService
+      )?.service_name || '';
+    const user_t_subserviceName =
+      this.subServiceList.find(
+        (e) => e.subService_id == this.testForm.value.subService
+      )?.subService_name || '';
+    const user_t_date = this.testForm.value.date;
+    this.userService
+      .getTestData(
+        user_t_name,
+        user_t_email,
+        user_t_testName,
+        user_t_testServiceName,
+        user_t_subserviceName,
+        user_t_date
+      )
+      .subscribe((respData: IDiagnosis[]) => {
+        alert('Test Appointment Successfull');
+        console.log(respData);
+      });
+  }
+
+  // onCheckboxChange(event: any) {
+  //   // this.checkboxList=this.testForm.value.subService
+  //   const checkname = event.target.value;
+  //   const isChecked = event.target.checked;
+  //   console.log(checkname);
+  //   this.checkboxList = this.get
+  // }
 }
