f94d50731148921f1cdd1307e3ff33df3a71540c
Deepika Mahindroo <deepika.mahindroo@sourcefuse.com>
2023-01-06 13:13:28 +0530

    lazy_loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppointmentComponent } from './components/appointment/appointment.component';
-import { CheckAppointmentComponent } from './components/check-appoitment/check-appointment.component';
-import { HomeComponent } from './components/home/home.component';
-import { LabTestsComponent } from './components/lab-tests/lab-tests.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { AuthGuard } from './guard/auth.guard';
-
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
   {
-    path: 'appointment',
-    component: AppointmentComponent,
-    canActivate: [AuthGuard],
+    path: '',
+    loadChildren: () =>
+      import('./admin/admin.module').then((mod) => mod.AdminModule),
   },
-  { path: 'labtests', component: LabTestsComponent, canActivate: [AuthGuard] },
+
   {
-    path: 'appointment-check',
-    component: CheckAppointmentComponent,
-    canActivate: [AuthGuard],
+    path: '',
+    loadChildren: () =>
+      import('./components/appointment/appointment.module').then(
+        (m) => m.AppointmentModule
+      ),
   },
 ];
 
---
+++ src/app/components/appointment/appointment-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppointmentComponent } from './appointment.component';
+import { AuthGuard } from 'src/app/guard/auth.guard';
+
+const routes: Routes = [
+  {
+    path: 'appointment',
+    component: AppointmentComponent,
+    canActivate: [AuthGuard],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AppointmentRoutingModule {}
